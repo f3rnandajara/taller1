7c45c4b5fe6eccbea8c6d5f6d4548f83315cd765
f3rnandajara <ferna.jara@hotmail.com>
2022-05-04 09:57:28 -0400

    Versión 1

---
+++ src/main/java/modelo/Estudiante.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+import java.util.ArrayList;
+import modelo.Ventana;
+
+/**
+ *
+ * @author fdaJara
+ */
+public class Estudiante {
+    
+    private String nombre;
+    private String matricula;
+    private String carrera;
+    private String correo;
+    
+        public Estudiante(String nombre, String matricula, String carrera, String correo)
+    {
+        this.nombre = nombre;
+        this.matricula = matricula;
+        this.carrera = carrera;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString(){
+        String cadena="";
+        cadena+= "Nombre: " + this.nombre +
+                "\nmatricula: " + this.matricula +
+                "\ncarrera: " + this.carrera +
+                "\ncorreo: " + this.correo;
+        return cadena;
+    }
+}
